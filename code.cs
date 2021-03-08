8207dd34529244b6c82478d277fcea7b9f7082f8
Gustavo F Reis, Nukkle <55332994+Nukkle134@users.noreply.github.com>
2021-03-08 17:34:32 -0300

    Update code.cs
    Improved the Landing Burn and Changed the Backflip Altitude.
    Now: Landing burn: ~2.8 m/s; backflip alt is: 1000m

--- code.cs
+++ code.cs
@@ -1,11 +1,13 @@
-using KRPC.Client;
 using KRPC.Client.Services.KRPC;
 using KRPC.Client.Services.SpaceCenter;
 using System;
 using System.Net;
 
+namespace ksp
 class Program
 {
+
+    class Program
     public static void Main()
     {
         using (var connection = new Connection(
@@ -17,6 +19,7 @@ class Program
             var krpc = connection.KRPC();
             Console.WriteLine(krpc.GetStatus().Version);
 
+        static void Main(string[] args)
         }
         bool reached_off_alture = false;
         bool reached_off_alture2 = false;
@@ -25,9 +28,18 @@ class Program
         bool Engine2Shutdown = false;
         bool Engine3Shutdown = false;
         bool ManuverStarted = false;
+        bool LandPitchSeq = false;
         bool StartLandMode = false;
         using (var connection = new Connection())
         {
+            // Conexão com o KRPC
+            Console.WriteLine($"Tentando conectar!");
+            var connection_ = new Connection(name: "kr2pc");
+            var krpc = connection_.KRPC();
+            bool reached_off_alture = false;
+            bool reached_off_alture2 = false;
+            bool pushed_last_burn = false;
+            using (var connection = new Connection())
             var spaceCenter = connection.SpaceCenter();
             var vessel = spaceCenter.ActiveVessel;
             var control = vessel.Control;
@@ -68,44 +80,122 @@ class Program
             control.ActivateNextStage();
             control.Throttle = 1;
             vessel.Flight();
+            void run()
+            {
+                var spaceCenter = connection.SpaceCenter();
+                var vessel = spaceCenter.ActiveVessel;
+                var control = vessel.Control;
+                vessel.Name = "SpaceFlightAcademy";
+                var srfFrame = vessel.Orbit.Body.ReferenceFrame;
+                var flightInfo = vessel.Flight();
+                System.Threading.Thread.Sleep(5000);
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\contagem.wav");
+                player.Play();
+                Console.WriteLine(flightInfo.MeanAltitude);
+                Console.WriteLine($"10");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"9");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"8");
+                Console.WriteLine("Landing Sequence Started");
+                System.Threading.Thread.Sleep(500);
+                vessel.AutoPilot.TargetPitch = 100;
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"7");
+                vessel.AutoPilot.TargetPitch = 84;
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"6");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"5");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"4");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"3");
+                control.Throttle = 1;
+                control.ActivateNextStage();
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"2");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"1");
+                vessel.AutoPilot.TargetPitch = 90;
+            }
+            bool acess_84 = false;
+            System.Threading.Thread T1 = new System.Threading.Thread(new System.Threading.ThreadStart(run));
+            void run2()
+            {
+                Console.WriteLine("Landing Sequence Started");
+                vessel.AutoPilot.TargetPitch = 84;
+                System.Threading.Thread.Sleep(5000);
+                vessel.AutoPilot.TargetPitch = 90;
+            }
+            System.Threading.Thread T2 = new System.Threading.Thread(new System.Threading.ThreadStart(run2));
             while (true)
             {
                 var srfSpeed = vessel.Flight(srfFrame).Speed;
                 if (flightInfo.MeanAltitude >= 2000 && flightInfo.MeanAltitude <= 3000 && !Engine1Shutdown)
                 {
                     // Desligar o primeiro motor
-                    var Engine1 = vessel.Parts.Engines[0];
+                    var Engine1 = vessel.Parts.Engines[2];
                     Engine1.Active = false;
-                    Console.WriteLine("Desligando motor 0");
+                    Console.WriteLine("Desligando motor 2");
                     Engine1Shutdown = true;
 
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"GO FOR LAUNCH!");
+                control.RCS = true;
+                control.Gear = false;
+                control.SAS = true;
+                control.SASMode = 0;
+                control.ActivateNextStage();
+                control.Throttle = 1;
+                vessel.Flight();
+                System.Threading.Thread.Sleep(10000);
+                System.Threading.Thread.Sleep(1000);
+                reached_off_alture = true;
+                System.Threading.Thread.Sleep(12500);
+                reached_off_alture2 = true;
+                while (true)
                 }
                 if (flightInfo.MeanAltitude >= 6000 && flightInfo.MeanAltitude <= 7000 && !Engine2Shutdown)
                 {
+                    if (reached_off_alture2 == true)
+                    {
+                        control.Parachutes = true;
+                        control.ActivateNextStage();
+                        System.Threading.Thread.Sleep(5000);
+                        control.Throttle = 0.00f;
+                        control.SASMode = SASMode.Radial;
+                        control.SetActionGroup(1, true);
+                        control.Brakes = true;
+                        break;
                     // Desligar o segundo motor
-                    var Engine1 = vessel.Parts.Engines[1];
+                    var Engine1 = vessel.Parts.Engines[0];
                     Engine1.Active = false;
-                    Console.WriteLine("Desligando motor 1");
+                    Console.WriteLine("Desligando motor 0");
                     control.Throttle = 0.45f;
                     Engine2Shutdown = true;
 
+                    }
+                    System.Threading.Thread.Sleep(0);
                 }
 
+                while (true)
                 if (flightInfo.MeanAltitude >= 9000 && flightInfo.MeanAltitude <= 10000 && !Engine3Shutdown)
                 {
                     // Desligar o terceiro motor
-                    var Engine1 = vessel.Parts.Engines[2];
+                    var Engine1 = vessel.Parts.Engines[1];
                     Engine1.Active = false;
                     Console.WriteLine("All Engines Shutdown");
                     Engine3Shutdown = true;
                 }
                 if (Engine1Shutdown && Engine2Shutdown && Engine3Shutdown && !ManuverStarted && srfSpeed <= 25)
                 {
+                    var srfSpeed = vessel.Flight(srfFrame).Speed;
                     BeginFlipManuver();
                     ManuverStarted = true;
                     break;
                 }
-                System.Threading.Thread.Sleep(5);
+                System.Threading.Thread.Sleep(50);
             }
 
             void BeginFlipManuver()
@@ -135,61 +225,103 @@ class Program
             }
             while (true)
             {
-                if (ManuverStarted == true && flightInfo.MeanAltitude < 425)
+                if (ManuverStarted == true && flightInfo.MeanAltitude < 1000)
                 {
+                    vessel.AutoPilot.TargetRoll = 0;
                     Console.WriteLine(flightInfo.Roll);
                     vessel.AutoPilot.TargetPitch = 120;
-                    vessel.AutoPilot.TargetRoll = 0;
-                    var flap3_ = vessel.Parts.ControlSurfaces[0];
-                    var flap4_ = vessel.Parts.ControlSurfaces[1];
+
+                    if (srfSpeed < 0.5 && pushed_last_burn == true)
+                    var flap3_ = vessel.Parts.ControlSurfaces[2];
+                    var flap4_ = vessel.Parts.ControlSurfaces[3];
                     flap4_.Deployed = false;
                     flap3_.Deployed = false;
                     control.SAS = true;
-                    control.Throttle = 1f;
+                    control.Throttle = 0.85f;
                     System.Threading.Thread.Sleep(5200);
                     if (flightInfo.HorizontalSpeed <= 0)
                     {
-                        vessel.AutoPilot.TargetPitch = 90;
+                        control.Throttle = 0;
+                        control.Brakes = false;
+                        System.Threading.Thread.Sleep(500);
+                        control.Gear = false;
                         StartLandMode = true;
+                        LandPitchSeq = true;
+                        T1.Name = "Landing Pitch Sequence";
+                        T1.Start();
                         break;
                     }
+                    if (flightInfo.MeanAltitude <= 800 && flightInfo.MeanAltitude >= 701 && reached_off_alture == true)
                     else
                     {
+                        control.Gear = true;
                         control.Throttle = 1f;
                     }
+                    else if (flightInfo.SurfaceAltitude <= 860)
                 }
                 System.Threading.Thread.Sleep(5);
 
             }
+            bool T2_Started = false;
 
             {
                 while (true)
                 {
                     if (StartLandMode == true)
                     {
+                        if (flightInfo.MeanAltitude <= 115)
+                        {
+                            T2_Started = true;
+                        }
 
                         var srfFrame2 = vessel.Orbit.Body.ReferenceFrame;
                         var srfSpeed2 = vessel.Flight(srfFrame).Speed;
-                        var Engine1_ = vessel.Parts.Engines[1];
+                        var Engine1_ = vessel.Parts.Engines[2];
                         Engine1_.Active = false;
-                        control.Roll = 6;
                         float increase = 0;
-                        Console.WriteLine((float)((1 * (Math.Sqrt(srfSpeed2 / 4)) / 1.6) / 1.1));
-                        control.Throttle = (float)((1 * (Math.Sqrt(srfSpeed2 / 4)) / 1.6) / 1.1);
-                        control.Gear = true;
+                        if (!T2_Started)
+                        {
+                            control.Throttle = (float)((1 * (Math.Sqrt(srfSpeed2 / 3)) / 1.6) / 2.2) + (increase);
+                            control.Gear = true;
+                            vessel.AutoPilot.TargetPitch = 88;
+                        }
+                        else
+                        {
 
-                        if ((float)((1 * (Math.Sqrt(srfSpeed2 / 4)) / 1.6) / 1.7) < 0.35 || flightInfo.SurfaceAltitude <= 5)
+                            float Throttle = (float)((1 * (Math.Sqrt(srfSpeed2 / 4)) / 1.6) / 1.08) + (increase);
+                            Console.WriteLine(Throttle);
+                            if (Throttle <= 0)
+                            {
+                                Throttle = 0.1f;
+                            }
+                            control.Throttle = Throttle;
+                            T2_Started = true;
+                            vessel.AutoPilot.TargetPitch = 90;
+
+                        }
+
+                        if ((float)((1 * (Math.Sqrt(srfSpeed2 / 4)) / 1.6) / 1.7) < 0.07 || flightInfo.SurfaceAltitude <= 5)
+
+                        Console.WriteLine("Throttle:" + (float)((1 * (Math.Sqrt(srfSpeed / 4)) / 1.5) / 1.2));
+                        control.Throttle = (float)((1 * (Math.Sqrt(srfSpeed / 4)) / 1.5) / 2);
+                        pushed_last_burn = true;
+                        if ((float)((1 * (Math.Sqrt(srfSpeed / 4)) / 1.5) / 1.77) < 0.26)
                         {
                             control.Throttle = 0;
                             control.Brakes = false;
+                            System.Threading.Thread.Sleep(1500);
+
                             break;
                         }
                     }
+                    System.Threading.Thread.Sleep(0);
                     System.Threading.Thread.Sleep(5);
                 }
 
 
             }
+            Console.WriteLine($"Connected Sucessfuly");
+            Console.ReadKey();
+
         }
     }
-}
